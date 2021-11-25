e5e7fb11669ac332cc8108fcb049d6dc880a14b7
Israhribeiro <rebiboboratavribeiro@gmail.com>
2021-11-24 21:44:07 -0300

    Programa principal

--- src/HashStudentSystem/Programa.java
+++ src/HashStudentSystem/Programa.java
@@ -1,7 +1,45 @@
 package HashStudentSystem;
 
+import java.util.Locale;
+import java.util.Scanner;
+import LSE_TADPilha.NodeStack;
+
 public class Programa {
     public static void main(String args[]){
-        System.out.println("Teste");
+
+        NodeStack studentsStack = new NodeStack();
+
+        Scanner in = new Scanner(System.in);
+
+        while(true){
+            int ra = Integer.parseInt(in.nextLine());
+            String nome = in.nextLine();
+            String endereco = in.nextLine();
+            String celular = in.nextLine().replace("-","").replace("(","").replace(")","").replace(" ", "");
+            String nascimento = in.nextLine();
+            System.out.println(ra+" "+nome+" "+endereco+" "+celular+" "+nascimento);
+
+            System.out.println("Você confirma as informações abaixo (s/n)?");
+            System.out.println("RA: "+ra);
+            System.out.println("Nome: "+nome);
+            System.out.println("Endereço: "+endereco);
+            System.out.println("Celular: "+celular);
+            System.out.println("Nascimento: "+nascimento);
+
+            String op = in.nextLine().toLowerCase(Locale.ROOT);
+
+            if(op == "s") {
+
+                studentsStack.push();
+
+                System.out.println("Você quer cadastrar outro aluno ?");
+                op = in.nextLine().toLowerCase(Locale.ROOT);
+
+                if(op != "s"){
+                    break;
+                }
+
+            }
+        }
     }
 }
