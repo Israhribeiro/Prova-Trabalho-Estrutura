3a8420d73239f9dd41d3d4276bb2b92c33fea773
erik-ozzetti-martins <erik.ozzetti.martins@gmail.com>
2021-11-24 21:45:04 -0300

    criacao de atributos  alunos

---
+++ src/aluno/Aluno.java
@@ -0,0 +1,55 @@
+package aluno;
+
+public class Aluno {
+
+    private int ra;
+
+    private String nome;
+
+    private String endereco;
+
+    private String celular;
+
+    private String dataDeNascimento;
+
+
+    public int getRa() {
+        return ra;
+    }
+
+    public void setRa(int ra) {
+        this.ra = ra;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(String dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+}
