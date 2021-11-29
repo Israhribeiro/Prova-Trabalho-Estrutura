d5ae24a7fe8e6d412641cf3df51a2c3d72f95a5b
schusmeicer <59938696+schusmeicer@users.noreply.github.com>
2021-11-28 22:18:15 -0300

    Feita criação das tratativas de inserção de dados errados. Utilizado try/catch para tratar os erros.

--- src/Funcoes/Funcoes.java
+++ src/Funcoes/Funcoes.java
@@ -4,6 +4,7 @@ import DicionarioHash.HashTableMultiMap;
 import LSE_TADPilha.NodeStack;
 import aluno.Aluno;
 
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -17,47 +18,56 @@ public class Funcoes {
 
         while(true){
             System.out.print("Insira o RA do Aluno: ");
-            int ra = Integer.parseInt(in.nextLine());
-            System.out.println();
-            System.out.print("Insira o Nome do Aluno: ");
-            String nome = in.nextLine();
-            System.out.println();
-            System.out.print("Insira o Endereço do Aluno: ");
-            String endereco = in.nextLine();
-            System.out.println();
-            System.out.print("Insira o Celular do Aluno: ");
-            String celular = in.nextLine().replace("-","").replace("(","").replace(")","").replace(" ", "");
-            System.out.println();
-            System.out.print("Insira a Data de Nascimento do Aluno: ");
-            String nascimento = in.nextLine();
+            try {
+                int ra = Integer.parseInt(in.nextLine());
+                System.out.println();
+                System.out.print("Insira o Nome do Aluno: ");
+                String nome = in.nextLine();
+                System.out.println();
+                System.out.print("Insira o Endereço do Aluno: ");
+                String endereco = in.nextLine();
+                System.out.println();
+                System.out.print("Insira o Celular do Aluno: ");
+                String celular = in.nextLine().replace("-","").replace("(","").replace(")","").replace(" ", "");
+                System.out.println();
+                System.out.print("Insira a Data de Nascimento do Aluno: ");
+                String nascimento = in.nextLine();
 
-            System.out.println("Você confirma as informações abaixo (s/n)?");
-            System.out.println("RA: "+ra);
-            System.out.println("Nome: "+nome);
-            System.out.println("Endereço: "+endereco);
-            System.out.println("Celular: "+celular);
-            System.out.println("Nascimento: "+nascimento);
+                System.out.println("Você confirma as informações abaixo (s/n)?");
+                System.out.println("RA: "+ ra);
+                System.out.println("Nome: "+nome);
+                System.out.println("Endereço: "+endereco);
+                System.out.println("Celular: "+celular);
+                System.out.println("Nascimento: "+nascimento);
 
-            String op = in.nextLine();
-            System.out.println();
+                String op = in.nextLine();
+                System.out.println();
 
-            if(op.compareTo("s") == 0) {
-                Aluno aluno = new Aluno(ra,nome,endereco,celular,nascimento);
+                if(op.compareTo("s") == 0) {
+                    Aluno aluno = new Aluno(ra,nome,endereco,celular,nascimento);
 
-                studentsStack.push(aluno);
+                    studentsStack.push(aluno);
 
-                System.out.println("Você quer cadastrar outro aluno ?");
-                op = in.nextLine().toLowerCase(Locale.ROOT);
+                    System.out.println("Você quer cadastrar outro aluno ?");
+                    op = in.nextLine().toLowerCase(Locale.ROOT);
 
                     if(op.equals("s") == false){
-                    for (int i = 0; i <= studentsStack.size(); i++) {
-                        Aluno alunoItem = studentsStack.pop();
-                        db.put(alunoItem.getRa(),alunoItem);
+                        for (int i = 0; i <= studentsStack.size(); i++) {
+                            Aluno alunoItem = studentsStack.pop();
+                            db.put(alunoItem.getRa(),alunoItem);
+                        }
+                        break;
                     }
-                    break;
-                }
 
+                }
+            }catch(NumberFormatException nexc){
+                System.out.println("Valor digitado não corresponde a um RA");
+                break;
+            }catch(InputMismatchException nexc){
+                System.out.println("Valor digitado não corresponde a um RA");
+                break;
             }
+
         }
     }
 
